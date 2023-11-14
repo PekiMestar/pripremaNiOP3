3c5439d266f52c089f8414d5740fc66b4dfed1c2
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-11-14 20:09:04 +0100

    Add files via upload

---
+++ pripremaNiOP3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripremaNiOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj;
+
+            Console.WriteLine("Unesite neki broj: ");
+            broj = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < broj; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.WriteLine(j);
+                }
+
+                Console.WriteLine(" ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
